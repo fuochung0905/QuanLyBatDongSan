c63b728223acc65aeca0537bc42da47bd10985b6
nguyenhung0905 <hung.nguyen@dgvdigital.com>
2024-12-06 03:07:23 -0800

    FE

--- ProjectManager/SimpleCQRS/SimpleCQRS/Program.cs
+++ ProjectManager/SimpleCQRS/SimpleCQRS/Program.cs
@@ -24,10 +24,9 @@ try
     });
     builder.Services.AddHttpContextAccessor();
     builder.Services.AddControllers();
-    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
-
     var app = builder.Build();
 
     // Configure the HTTP request pipeline.
@@ -36,12 +35,13 @@ try
         app.UseDeveloperExceptionPage();
         app.UseMigrationsEndPoint();
         app.UseForwardedHeaders();
+        app.UseSwagger();
+        app.UseSwaggerUI();
     }
     else
     {
         await app.InitialDatabaseAsync();
     }
-    await app.InitialDatabaseAsync();
     app.UseHttpsRedirection();
     app.UseStaticFiles();
 
---
+++ ProjectManager/frontend/src/components/layout/LayoutBody.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LayoutBody = () =>{
+    return(
+    <div className="col-md-9">
+        <h3>Main content</h3>
+        <p>This is the main content area</p>
+    </div>
+    );
+};
+export default LayoutBody;
